cf80c4f78e4fceb46b47496d9dd272556891a0f4
Ouch1978 <ouch1978@hotmail.com>
2017-05-08 16:28:37 +0800

    First Commit

---
+++ PlayXamarinFormsWithGorilla/PlayXamarinFormsWithGorilla/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PlayXamarinFormsWithGorilla
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_OnItemSelected( object sender , SelectedItemChangedEventArgs e )
+        {
+            if( e.SelectedItem != null )
+            {
+                var detailPage = new DetailPage( e.SelectedItem as Book );
+
+                Navigation.PushAsync( detailPage );
+
+            }
+        }
+
+    }
+}
---
+++ PlayXamarinFormsWithGorilla/PlayXamarinFormsWithGorilla/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayXamarinFormsWithGorilla.Models
+{
+    public partial class Book
+    {
+        public string Name { get; set; }
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public System.DateTime ReleaseDate { get; set; }
+        public string CoverUri { get; set; }
+    }
+}
